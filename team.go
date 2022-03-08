04079ad9c8b11738e4694dd7d1517f2124e6dea3
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2022-03-08 11:45:51 +0200

    fixed a bug where bot cannot send message if txt field is more than 2000 in lenght

--- team.go
+++ team.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	s "github.com/jlehtimaki/toornament-csgo/pkg/structs"
 	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 func (d *discordBot) team(teamName string) {
@@ -33,7 +34,7 @@ func (d *discordBot) team(teamName string) {
 	// Players string
 	var playerString string
 	for _, p := range team.Players {
-		pTmpString := fmt.Sprintf("\t**%s (%s)** \n"+
+		pTmpString := fmt.Sprintf("> %s (%s)\n"+
 			"\t\t Faceit:\n"+
 			"\t\t\t Rank/Elo/Most Played/Least Played/KD:\n"+
 			"\t\t\t\t %d/%d/%s/%s/%s \n"+
@@ -42,7 +43,7 @@ func (d *discordBot) team(teamName string) {
 			"\t\t MM Rank: %s\n"+
 			"\t\t Kanaliiga:\n"+
 			"\t\t\t Rating/ADR/Kills/Assists/Deaths\n"+
-			"\t\t\t\t %.2f/%.2f/%d/%d/%d \n",
+			"\t\t\t %.2f/%.2f/%d/%d/%d \n",
 			p.Name, p.CustomFields.SteamId,
 			p.Faceit.Rank, p.Faceit.Elo, p.Faceit.MostPlayedMap.Name, p.Faceit.LeastPlayedMap.Name, p.Faceit.KD,
 			p.Esportal.Rank,
@@ -58,7 +59,7 @@ func (d *discordBot) team(teamName string) {
 		"***Best Map***: %s (%s%%)\n"+
 		"***Worst Map***: %s (%s%%)\n\n"+
 		"***Lineup:***: \n"+
-		"%s"+
+		"```%s```"+
 		"",
 		team.Name,
 		team.CustomFields.CaptainDiscord,
@@ -66,6 +67,37 @@ func (d *discordBot) team(teamName string) {
 		team.WorstMap.Name, team.WorstMap.WinRate,
 		playerString)
 
+	if len(teamString) >= 2000 {
+		teamStringSplit := strings.Split(teamString, "```")
+		for _, ts := range teamStringSplit {
+			if ts == "" {
+				continue
+			}
+			if len(ts) >= 2000 {
+				playerSplit := strings.Split(ts, ">")
+				for n, ps := range playerSplit {
+					if n == 0 {
+						continue
+					}
+					_, err = d.s.ChannelMessageSend(channel.ID, ps)
+					if err != nil {
+						log.Info("fooo")
+						d.s.ChannelMessageSend(channel.ID, "sorry could not get that for you")
+						log.Error(err)
+						return
+					}
+				}
+			} else {
+				_, err = d.s.ChannelMessageSend(channel.ID, ts)
+				if err != nil {
+					d.s.ChannelMessageSend(channel.ID, "sorry could not get that for you")
+					log.Error(err)
+					return
+				}
+			}
+		}
+		return
+	}
 	_, err = d.s.ChannelMessageSend(channel.ID, teamString)
 	if err != nil {
 		d.s.ChannelMessageSend(channel.ID, "sorry could not get that for you")
